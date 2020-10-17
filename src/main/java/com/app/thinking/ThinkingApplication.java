1710cbbf571fe7104aba1afc66a91ed1d43342d4
Prem kiran <premkiran3225@gmail.com>
2020-10-17 10:12:28 -0400

    Create .war file

--- src/main/java/com/app/thinking/ThinkingApplication.java
+++ src/main/java/com/app/thinking/ThinkingApplication.java
@@ -2,12 +2,19 @@ package com.app.thinking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThinkingApplication {
+public class ThinkingApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThinkingApplication.class, args);
 	}
+	 
+	 @Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(ThinkingApplication.class);
+	 }
 
 }
