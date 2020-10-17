c14091455749fee114da96abe6688ecadb03d3d4
akhil-coder7 <ravipati0102@gmail.com>
2020-10-17 01:11:38 -0400

    Modified

--- src/main/java/com/app/thinking/controller/ThinkingController.java
+++ src/main/java/com/app/thinking/controller/ThinkingController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ThinkingController {
 
-	@GetMapping("/")
+	@GetMapping("/open")
 	public String homePage() {
 		return "Welcome to Spring Boot API";
 	}
